6da4a50b7d0480c8f205fde58edd0fedf7730f40
Burhan <burhan.ayub@gmail.com>
2020-06-17 19:50:49 -0700

    Working StreamEdit using URL based navigation

---
+++ client/src/history.js
@@ -0,0 +1,3 @@
+import { createBrowserHistory } from 'history'; 
+export default createBrowserHistory();
+// We are maintaining our own history object instead of using BrowserRouter's history object
